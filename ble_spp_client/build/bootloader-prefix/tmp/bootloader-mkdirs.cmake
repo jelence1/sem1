29cbcad572049f0f4e4e6f3efea1b9a2ab871e7b
jelence1 <jelena.gavran@fer.hr>
2023-01-17 19:57:46 +0100

    sem1

---
+++ ble_spp_client/build/bootloader-prefix/tmp/bootloader-mkdirs.cmake
@@ -0,0 +1,19 @@
+# Distributed under the OSI-approved BSD 3-Clause License.  See accompanying
+# file Copyright.txt or https://cmake.org/licensing for details.
+
+cmake_minimum_required(VERSION 3.5)
+
+file(MAKE_DIRECTORY
+  "C:/Espressif/frameworks/esp-idf-v4.4/components/bootloader/subproject"
+  "D:/JELENA/FAKS/4.godina/1.semestar/sem1/ble_spp_client/build/bootloader"
+  "D:/JELENA/FAKS/4.godina/1.semestar/sem1/ble_spp_client/build/bootloader-prefix"
+  "D:/JELENA/FAKS/4.godina/1.semestar/sem1/ble_spp_client/build/bootloader-prefix/tmp"
+  "D:/JELENA/FAKS/4.godina/1.semestar/sem1/ble_spp_client/build/bootloader-prefix/src/bootloader-stamp"
+  "D:/JELENA/FAKS/4.godina/1.semestar/sem1/ble_spp_client/build/bootloader-prefix/src"
+  "D:/JELENA/FAKS/4.godina/1.semestar/sem1/ble_spp_client/build/bootloader-prefix/src/bootloader-stamp"
+)
+
+set(configSubDirs )
+foreach(subDir IN LISTS configSubDirs)
+    file(MAKE_DIRECTORY "D:/JELENA/FAKS/4.godina/1.semestar/sem1/ble_spp_client/build/bootloader-prefix/src/bootloader-stamp/${subDir}")
+endforeach()
